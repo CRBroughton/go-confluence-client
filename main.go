bcc0c5a805e7e58657a6ddfcf38b30f6f526c5b0
CRBroughton <crbroughton@posteo.uk>
2024-10-25 18:33:29 +0100

    feat: :sparkles: add UpdatePageByID method

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package goconfluenceclient
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -40,9 +41,11 @@ type PageStorage struct {
 }
 
 type PageVersion struct {
-	Number int `json:"number"`
+	Number  int    `json:"number"`
+	Message string `json:"message"`
 }
 
+// Retrieves a Confluence page via it's ID
 func (apiClient *APIClient) GetPageByID(pageID string) (*Page, error) {
 	url := fmt.Sprintf("%s/wiki/api/v2/pages/%s?body-format=storage", apiClient.Url, pageID)
 
@@ -77,4 +80,62 @@ func (apiClient *APIClient) GetPageByID(pageID string) (*Page, error) {
 	}
 
 	return &page, nil
+}
+
+func (apiClient *APIClient) UpdatePageByID(pageID, title, pageBody string, versionNumber int, versionMessage string) (*Page, error) {
+	url := fmt.Sprintf("%s/wiki/api/v2/pages/%s", apiClient.Url, pageID)
+
+	pageData := Page{
+		ID:     pageID,
+		Status: "current",
+		Title:  title,
+		Body: PageBody{
+			Storage: PageStorage{
+				Value:          pageBody,
+				Representation: "storage",
+			},
+		},
+		Version: PageVersion{
+			Number:  versionNumber,
+			Message: versionMessage,
+		},
+	}
+
+	jsonData, err := json.Marshal(pageData)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, err
+	}
+
+	req.SetBasicAuth(apiClient.Email, apiClient.ApiToken)
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to update the confluence page: status code %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var updatedPage Page
+	err = json.Unmarshal(body, &updatedPage)
+	if err != nil {
+		return nil, err
+	}
+
+	return &updatedPage, nil
 }
